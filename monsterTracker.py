ea6b9ca931e9682bc5ae09b9ab209a3e2265a377
emanbu <31211226+emilliegiraf@users.noreply.github.com>
2018-12-11 15:44:35 +0100

    Fully functioning with 1 monster, on towards several

--- monsterTracker.py
+++ monsterTracker.py
@@ -1,9 +1,7 @@
-def init_monster(name, ac, hp):
-    monster = {'name': name, 'ac': ac, 'hp': hp}
-
 class monster:
     def __init__(self, name, ac, hp):
         self.name = name
+        self.bloodied = False
         self.dead = False
         self.ac = ac
         self.half_max_hp = hp / 2
@@ -11,24 +9,28 @@ class monster:
 
     def print_monster(self):
         print("The monster", self.name, "with AC", self.ac, "has", self.hp,
-        "HP left.")
+              "HP left.")
 
-    def hit_monster(self, roll):
-        if (not(roll < self.ac)):
-            return True
+    def hit_monster(self):
+        roll = int(input("Roll to hit the monster: "))
+        if (roll >= self.ac):
+            self.hurt_monster()
         else:
-            return False
+            print("You did not hit.")
 
-    def hurt_monster(self, damage):
+    def hurt_monster(self):
+        damage = int(input("Roll your damage: "))
         self.hp -= damage
         if (self.hp < 0):
             self.dead = True
-            return 3
-        elif ((self.hp + damage) > self.half_max_hp and
-                self.hp < self.half_max_hp):
-            return 2
+            print("The monster", self.name, "is dead!")
+        elif ((self.hp + damage) > self.half_max_hp
+              and self.hp <= self.half_max_hp):
+            self.bloodied = True
+            print("The monster", self.name, "is bloodied.")
         else:
-            return 1
+            print("The monster has been damaged.")
+
 
 def main():
     #name = input("Let's slay a monster! I will need these things:\nName: ")
@@ -37,11 +39,7 @@ def main():
     monster1 = monster('Ken', 12, 30)
     while (not monster1.dead):
         monster1.print_monster()
-        roll = int(input("Roll to hit the monster: "))
-        if (monster1.hit_monster(roll)):
-            dmg = int(input("How much damage does the monster take? "))
-            monster1.hurt_monster(dmg)
-        else:
-            print("That does not hit :(")
+        monster1.hit_monster()
+
 
 main()
